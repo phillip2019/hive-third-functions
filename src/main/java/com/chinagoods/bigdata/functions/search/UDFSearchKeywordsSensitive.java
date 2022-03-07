eb2c36a443e0559a5d71081c4495a31cef4fe722
xiaowei.song <songxiaowei@chinagoods.com>
2022-03-07 16:11:19 +0800

    #Updates#添加违禁词UDF函数

---
+++ src/main/java/com/chinagoods/bigdata/functions/search/UDFSearchKeywordsSensitive.java
@@ -0,0 +1,94 @@
+package com.chinagoods.bigdata.functions.search;
+
+import com.chinagoods.bigdata.functions.utils.MysqlUtil;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.io.Text;
+
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author xiaowei.song
+ * date: 2022-03-07
+ * time: 14:48
+ */
+@Description(name = "search_keywords_sensitive", value = "_FUNC_(str) - Determines whether the keyword is a prohibited word, if it returns 1, otherwise returns 0. "
+        , extended = "Example:\n"
+        + "  > SELECT _FUNC_(key_word) FROM src LIMIT 1;")
+public class UDFSearchKeywordsSensitive extends GenericUDF {
+    private ObjectInspectorConverters.Converter[] converters;
+
+    /**
+     * Number of arguments to this UDF
+     **/
+    private static final int ARG_COUNT = 1;
+
+    /**
+     * 搜索关键词禁用表
+     **/
+    public Set<String> keywordsSensitive = new HashSet<String>();
+
+    public UDFSearchKeywordsSensitive() {
+    }
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+        if (arguments.length != ARG_COUNT) {
+            throw new UDFArgumentLengthException(
+                    "The function search_keywords_sensitive(key_word) takes exactly " + ARG_COUNT + " arguments.");
+        }
+
+        converters = new ObjectInspectorConverters.Converter[arguments.length];
+        for (int i = 0; i < arguments.length; i++) {
+            converters[i] = ObjectInspectorConverters.getConverter(arguments[i],
+                    PrimitiveObjectInspectorFactory.writableStringObjectInspector);
+        }
+
+        // 查询现有搜索引擎禁用词
+        MysqlUtil mysqlUtil = new MysqlUtil();
+        try {
+            keywordsSensitive = mysqlUtil.getSearchSensitiveKeywords();
+        } catch (SQLException e) {
+            throw new UDFArgumentException(String.format("Failed to query the set of prohibited words in the search engine database, the error details are: %s", e));
+        }
+
+        return PrimitiveObjectInspectorFactory
+                .writableIntObjectInspector;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
+        assert (arguments.length == ARG_COUNT);
+
+        if (arguments[0].get() == null || StringUtils.isBlank((String) arguments[0].get())) {
+            return 0;
+        }
+
+        try {
+            Text keywords = (Text) converters[0].convert(arguments[0].get());
+            for (String sensitiveKeyword : keywordsSensitive) {
+                if (StringUtils.contains(keywords.toString(), sensitiveKeyword)) {
+                    return 1;
+                }
+            }
+            return 0;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    @Override
+    public String getDisplayString(String[] strings) {
+        assert (strings.length == ARG_COUNT);
+        return "search_keywords_sensitive(" + strings[0] + ")";
+    }
+}
---
+++ src/main/java/com/chinagoods/bigdata/functions/utils/MysqlUtil.java
@@ -0,0 +1,80 @@
+package com.chinagoods.bigdata.functions.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author xiaowei.song
+ */
+public class MysqlUtil {
+    private static final Logger logger = LoggerFactory.getLogger(MysqlUtil.class);
+    private Connection connection;
+    public static final String DB_URL = "jdbc:mysql://172.18.7.7:3306/cg_search?characterEncoding=UTF-8&useSSL=false";
+    public static final String DB_USER = "cg_search";
+    public static final String DB_PASSWORD = "GPuBoTWz3UiMwwLz";
+
+    public static final String SELECT_SENSITIVE_KEYWORDS_SQL = "select words key_word\n" +
+            "from lexicon_sensitive ls\n" +
+            "where 1=1\n" +
+            "and status = 0\n" +
+            "and is_deleted = 0";
+    /**
+     * MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
+     **/
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+    public MysqlUtil() {
+        connection = getConnection();
+    }
+
+    public Connection getConnection(){
+        Connection conn = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        }catch (Exception e) {
+            logger.error("获取mysql连接失败, dbUrl: {}, username: {}, password: {}", DB_URL, DB_USER, DB_PASSWORD, e);
+            System.exit(1);
+        }
+        return conn;
+    }
+
+    /**
+     * 查询表中关键词
+     * @throws SQLException 查詢異常
+     */
+     public Set<String> getSearchSensitiveKeywords() throws SQLException {
+        Set<String> keywordsSet = new HashSet<>(3000);
+        // 重建mysql连接信息
+        if (connection == null || connection.isClosed()) {
+             connection = getConnection();
+         }
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(SELECT_SENSITIVE_KEYWORDS_SQL);
+        // 展开结果集数据库
+        while(rs.next()) {
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                keywordsSet.add((String) rs.getObject(i + 1));
+            }
+        }
+        // 完成后关闭
+        rs.close();
+        stmt.close();
+        return keywordsSet;
+    }
+
+    public void close() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        MysqlUtil mysqlUtil = new MysqlUtil();
+        System.out.println(mysqlUtil.getSearchSensitiveKeywords());
+    }
+}
