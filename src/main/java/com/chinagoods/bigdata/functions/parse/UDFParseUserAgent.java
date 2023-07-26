710016fc04713477672efb1e1857780b75e75305
xiaowei.song <songxiaowei@chinagoods.com>
2023-07-26 10:41:03 +0800

    #Updates#浏览器ua解析优化,使用guava缓存结果，效果提升杠杠的

--- src/main/java/com/chinagoods/bigdata/functions/parse/UDFParseUserAgent.java
+++ src/main/java/com/chinagoods/bigdata/functions/parse/UDFParseUserAgent.java
@@ -1,5 +1,8 @@
 package com.chinagoods.bigdata.functions.parse;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
@@ -18,7 +21,10 @@ import ua_parser.Parser;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zyl
@@ -40,7 +46,15 @@ public class UDFParseUserAgent  extends GenericUDF {
     public static final String UNKNOWN_UA_STR = "AppName";
     public static final String SEMICOLON_SEP = ";";
     public static final String UNKNOWN_STR = "unknown";
+    public static final String RST_UNKNOWN_STR = "unknown,unknown,unknown,unknown,unknown,unknown";
     private Parser uaParser = new Parser();
+
+    private CacheLoader<String, String> uaLoader = null;
+
+    public LoadingCache<String, String> uaCache = null;
+
+
+
     private ObjectInspectorConverters.Converter[] converters;
 
     public UDFParseUserAgent() {}
@@ -48,7 +62,7 @@ public class UDFParseUserAgent  extends GenericUDF {
     @Override
     public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
         if (arguments.length != ARG_COUNT) {
-            throw new UDFArgumentException(
+            throw new UDFArgumentException (
                     "The function parse_ua takes exactly " + ARG_COUNT + " arguments.");
         }
         converters = new ObjectInspectorConverters.Converter[arguments.length];
@@ -56,34 +70,69 @@ public class UDFParseUserAgent  extends GenericUDF {
             converters[i] = ObjectInspectorConverters.getConverter(arguments[i],
                     PrimitiveObjectInspectorFactory.javaStringObjectInspector);
         }
-        return ObjectInspectorFactory.getStandardListObjectInspector(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+
+        uaLoader = new CacheLoader<String, String>() {
+            @Override
+            public String load(String key) {
+                // 缓存miss时,加载数据的方法
+                logger.info("进入加载数据, key： {}", key);
+                return uaParse(key);
+            }
+        };
+        uaCache = CacheBuilder.newBuilder()
+                .maximumSize(10000)
+                //缓存项在给定时间内没有被写访问（创建或覆盖），则回收。如果认为缓存数据总是在固定时候后变得陈旧不可用，这种回收方式是可取的。
+                .expireAfterAccess(5, TimeUnit.MINUTES)
+                .build(uaLoader);
+
+//        return ObjectInspectorFactory.getStandardListObjectInspector(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+        return PrimitiveObjectInspectorFactory.javaStringObjectInspector;
     }
 
     @Override
-    public ArrayList<String> evaluate(DeferredObject[] arguments) throws HiveException {
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
         ArrayList<String> result = new ArrayList<>(RET_ARRAY_SIZE);
         assert (arguments.length == ARG_COUNT);
         String uaStr = converters[0].convert(arguments[0].get()).toString();
+        String rstUaStr = RST_UNKNOWN_STR;
+        try {
+            rstUaStr = uaCache.get(uaStr);
+        } catch (ExecutionException e) {
+            logger.error("缓存获取失败，原始UA为: {}", uaStr, e);
+        }
+        return rstUaStr;
+    }
 
+    private String uaParse(String uaStr) {
+        ArrayList<String> rstList = new ArrayList<>(RET_ARRAY_SIZE);
         // 默认值设置为null
         if (StringUtils.isBlank(uaStr) || StringUtils.equals(BLANK_UA_STR, uaStr) || StringUtils.equals(UNKNOWN_UA_STR, uaStr)) {
-            initEmptyArray(result);
-            return result;
+            initEmptyArray(rstList);
+            return StringUtils.join(rstList, ",");
         }
-
         // 0: 设备硬件型号 1: 操作系统型号 2: 操作系统小版本 3: 操作系统大版本 4: 浏览器小版本 5: 浏览器大版本
-        String deviceModel = "";
-        String osName = "";
-        String osVersion = "";
-        String osVersionName = "";
-        String packageVersion = "";
-        String packageName = "";
+        String deviceModel = UNKNOWN_STR;
+        String osName = UNKNOWN_STR;
+        String osVersion = UNKNOWN_STR;
+        String osVersionName = UNKNOWN_STR;
+        String packageVersion = UNKNOWN_STR;
+        String packageName = UNKNOWN_STR;
 
         // 添加默认值
         if (!StringUtils.startsWith(uaStr, ANDROID_UA_PREFIX) && !StringUtils.startsWith(uaStr, IOS_UA_PREFIX)) {
             // 清除默认值
             // 解析UA
-            Client c = uaParser.parse(uaStr);
+            Client c = null;
+            try {
+                c = uaParser.parse(uaStr);
+            } catch (Exception e) {
+                logger.error("解析UA失败，原始UA内容为: {}", uaStr);
+            }
+
+            if (Objects.isNull(c)) {
+                initEmptyArray(rstList);
+                return StringUtils.join(rstList, ",");
+            }
             // 0: 设备硬件型号 1: 操作系统型号 2: 操作系统小版本 3: 操作系统大版本 4: 浏览器小版本 5: 浏览器大版本
             deviceModel = formatValue(Optional.of(c.device).orElse(null).family);
             osName = formatValue(Optional.of(c.os).orElse(null).family);
@@ -102,6 +151,7 @@ public class UDFParseUserAgent  extends GenericUDF {
             // 正则切分
             try {
                 // com.scgroup.shopmall/1.2.3;Android ELE-AL00;U;OS 10;zh
+//                com.scgroup.shopmall/2.2.7;iOS iPhone X
                 String[] uaArr = uaStr.split(SEMICOLON_SEP);
                 String uaPackageVersion = uaArr[0];
                 String[] uaPvArr = uaPackageVersion.split("/");
@@ -117,13 +167,17 @@ public class UDFParseUserAgent  extends GenericUDF {
                 if (uaOsDeviceFmParams.length > 1) {
                     deviceModel = uaOsDeviceFmParams[1];
                 }
-                osVersionName = uaArr[2];
-                osVersion = uaArr[3];
+
+                if (uaArr.length > 2) {
+                    osVersionName = uaArr[2];
+                    osVersion = uaArr[3];
+                }
             } catch (Exception e) {
                 // 异常处理
-                 logger.error("解析android UA错误, ua = {}", uaStr, e);
+                logger.error("解析android UA错误, ua = {}", uaStr, e);
             }
         } else if (StringUtils.startsWith(uaStr, IOS_UA_PREFIX)) {
+            // 处理旧版本ua
             // 特殊ua
             // com.ccc.chinagoodsbuyer/1.3.0;iOS
             if (StringUtils.equals(uaStr.trim(), "com.ccc.chinagoodsbuyer/1.3.0;iOS")) {
@@ -153,19 +207,20 @@ public class UDFParseUserAgent  extends GenericUDF {
         }
 
         // 清除默认值
-        result.add(deviceModel);
-        result.add(osName);
-        result.add(osVersion);
-        result.add(osVersionName);
-        result.add(packageVersion);
-        result.add(packageName);
-        return result;
+        rstList.add(deviceModel);
+        rstList.add(osName);
+        rstList.add(osVersion);
+        rstList.add(osVersionName);
+        rstList.add(packageVersion);
+        rstList.add(packageName);
+        String rstUaStr = StringUtils.join(rstList, ",");
+        return rstUaStr;
     }
 
     private void initEmptyArray(List<String> result) {
         result.clear();
         for (int i = 0; i < RET_ARRAY_SIZE; i++) {
-            result.add(null);
+            result.add(UNKNOWN_STR);
         }
     }
 
@@ -181,8 +236,23 @@ public class UDFParseUserAgent  extends GenericUDF {
 
     public static void main(String[] args) throws HiveException {
 //        String uaStr = "Mozilla/5.0 (Linux; Android 12; NOH-AN00 Build/HUAWEINOH-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5131 MMWEBSDK/20230504 MMWEBID/9466 MicroMessenger/8.0.37.2380(0x2800255B) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64";
+        String uaStr = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; FTDv3 Browser; .NET CLR 1.0.3705; AvaliablePing)";
 //        String uaStr = "com.scgroup.shopmall/1.2.3 (Android ELE-AL00; U; OS 10; zh)";
-        String uaStr = "com.ccc.chinagoodsbuyer/2.0.1 (iOS unknown; 16.3.1; zh)";
+//        String uaStr = "com.ccc.chinagoodsbuyer/2.0.1 (iOS unknown; 16.3.1; zh)";
+//        String uaStr = "com.scgroup.shopmall/2.2.7;iOS iPhone X";
+//        String uaStr = "Opera/6.0 (Windows XP; U)  [de]";
+        long begin = System.currentTimeMillis();
+//        UDFParseUserAgent urlFormat = new UDFParseUserAgent();
+//        DeferredObject[] deferredObjects = new DeferredObject[2];
+//        // 平台类型、sc_url
+//        deferredObjects[0] = new DeferredJavaObject(uaStr);
+//
+//        ObjectInspector[] inspectorArr = new ObjectInspector[1];
+//        inspectorArr[0] = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
+//        urlFormat.initialize(inspectorArr);
+//        Object retArr = urlFormat.evaluate(deferredObjects);
+//        System.out.println(retArr);
+
         UDFParseUserAgent urlFormat = new UDFParseUserAgent();
         DeferredObject[] deferredObjects = new DeferredObject[2];
         // 平台类型、sc_url
@@ -191,8 +261,13 @@ public class UDFParseUserAgent  extends GenericUDF {
         ObjectInspector[] inspectorArr = new ObjectInspector[1];
         inspectorArr[0] = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
         urlFormat.initialize(inspectorArr);
-        ArrayList<String> retArr = urlFormat.evaluate(deferredObjects);
-        System.out.println(retArr);
+        for (int i = 0; i < 10000; i++) {
+            Object retArr = urlFormat.evaluate(deferredObjects);
+//            System.out.println(retArr);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("测试1耗时："+ (end - begin) + "ms");
+
 //        Parser uaParser = new Parser();
 //        Client c = uaParser.parse(uaStr);
 //        logger.info(Optional.of(c.device).orElseGet(null).family);
