0f314633732930cd74a4e1484c2ba7462ddc6036
xiaowei.song <songxiaowei@chinagoods.com>
2024-02-01 13:34:40 +0800

    #Updates#标准url修改地址

--- src/main/java/com/chinagoods/bigdata/functions/url/UDFStandardUrlFormat.java
+++ src/main/java/com/chinagoods/bigdata/functions/url/UDFStandardUrlFormat.java
@@ -36,7 +36,7 @@ import java.util.regex.Pattern;
         , extended = "Example:\n> SELECT _FUNC_(platform_type,sc_url) FROM src;")
 public class UDFStandardUrlFormat extends GenericUDF {
     private static final Logger logger = LoggerFactory.getLogger(UDFStandardUrlFormat.class);
-    private static final String DB_URL = "jdbc:mysql://172.18.5.10:23307/source?characterEncoding=UTF-8&useSSL=false";
+    private static final String DB_URL = "jdbc:mysql://172.18.5.22:3306/source?characterEncoding=UTF-8&useSSL=false";
     private static final String DB_USER = "source";
     private static final String DB_PASSWORD = "jP8*dKw,bRjBVos=";
     /**
